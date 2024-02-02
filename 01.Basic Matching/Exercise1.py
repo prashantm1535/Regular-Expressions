dcc9e1f55848caed2875da5a2db25cad87f5555e
P1535 <plm1535172@gmail.com>
2024-02-02 21:44:12 +0530

    finding specific word

--- 01.Basic Matching/Exercise1.py
+++ 01.Basic Matching/Exercise1.py
@@ -1,3 +1,8 @@
+""" Finding Specific Word :
+To locate the character or word you're searching for, just type it directly, much like a regular search. 
+For instance, if you're looking for the word curious in the text, enter it exactly as is.
+Note: Always use raw string! """
+
 # Import the necessary library
 import re
 
